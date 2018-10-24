24a6d8e34fb4d2dded1217b5e87727fcbd1fd9db
Mateusz Sawka <mateusz@marketmeme.com>
2018-10-24 13:10:35 -0700

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack'); 
 
 module.exports = (env) => {
  const isProduction = env === 'production';
