7f4aeb15e790e5827e460e112e2f59dea4fef57a
Mateusz Sawka <mateusz@marketmeme.com>
2018-11-05 13:48:06 -0800

    bug fix: set form to page 1

--- src/component/BasicWizard.js
+++ src/component/BasicWizard.js
@@ -11,7 +11,7 @@ export default class BasicWizard extends React.Component {
     constructor(props) {
         super(props);
       this.state = {
-        currentStep: 4,
+        currentStep: 1,
         currentData: [],
         uniqueId: ''
       };
